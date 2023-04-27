37f7239555f4db08b43105c8f18a58bf1460cbff
aktiangco <113158472+aktiangco@users.noreply.github.com>
2023-04-26 18:58:52 -0700

    done

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -28,32 +28,31 @@ router.post('/', async (req, res) => {
 
 // route handler to respond to that fetch request
 router.get('/profile', async (req, res) => {
-    
     try {
-        // Split the authorization header into [ "Bearer", "TOKEN" ]:
-        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+            // Split the authorization header into [ "Bearer", "TOKEN" ]:
+            const [method, token] = req.headers.authorization.split(' ')
         
-        // Only handle "Bearer" authorization for now 
-        //  (we could add other authorization strategies later):
-        if (authenticationMethod == 'Bearer') {
+            // Only handle "Bearer" authorization for now 
+            //  (we could add other authorization strategies later):
+            if (method == 'Bearer') {
             
-            // Decode the JWT
+                    // Decode the JWT
             const result = await jwt.decode(process.env.JWT_SECRET, token)
-            
+    
             // Get the logged in user's id from the payload
             const { id } = result.value
             
             // Find the user object using their id:
             let user = await User.findOne({
                 where: {
-                    userId: id
-                }
-            })           
-            res.json(user)
-        }
-    } catch (err){ 
-        res.json(null)
-    }
-})
-
-module.exports = router
+                        userId: id
+                    }
+                })           
+                res.json(user)
+            }
+        } catch (err){ 
+                res.json(null)
+            }
+        })
+        
+        module.exports = router
